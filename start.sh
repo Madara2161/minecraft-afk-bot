31e4f39538615548c0656e34a9c264ccc6651301
Madara2161 <153296867+Madara2161@users.noreply.github.com>
2024-03-24 02:57:23 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,14 +5,12 @@ ERRNO=${?}
 
 while (( $ERRNO == 0 )); do
 	echo "| Reconnexion dans 5 secondes"
-	sleep 5
 	node bot.js
 	ERRNO=${?}
 done
 
 while (($ERRNO == 60)); do
     echo "| Déconnecté, reconnexion dans 60 secondes, Ctrl+C pour annuler"
-	sleep 60
 	node bot.js
 	ERRNO=${?}
 done
